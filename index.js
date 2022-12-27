712750464efa78545c3c35127fc364a7ad346ad0
AkashChakrabortty <akashchakrabortty@gmail.com>
2022-12-27 17:34:52 +0600

    store gmail login user

--- index.js
+++ index.js
@@ -14,9 +14,29 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@clu
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run(){
+     const userCollection = client.db("Trending-com").collection("users");
     try{
 
-          
+      //insert userinfo into the database
+      app.post("/storeUser", async (req, res) => {
+        const userInfo = req.body;
+        // console.log(userInfo);
+        const filter = { email: userInfo.email };
+        const options = { upsert: true };
+        const updateDoc = {
+          $set: {
+            email: userInfo.email,
+            name: userInfo.name,
+            photoUrl: userInfo.photoUrl,
+          },
+        };
+       const result = await userCollection.updateOne(
+         filter,
+         updateDoc,
+         options
+       );
+        res.send(result);
+      });
 
     }
 
