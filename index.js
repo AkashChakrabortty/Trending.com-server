aca973dc1180dd2988d5b301019ee21d957210ab
AkashChakrabortty <akashchakrabortty@gmail.com>
2022-12-28 00:06:05 +0600

    post

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -11,40 +11,57 @@ app.use(express.json());
 
 // mongo
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.kusbv.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
-async function run(){
-     const userCollection = client.db("Trending-com").collection("users");
-    try{
+async function run() {
+  const userCollection = client.db("Trending-com").collection("users");
+  const postCollection = client.db("Trending-com").collection("posts");
+  try {
+    //insert userinfo into the database
+    app.post("/storeUser", async (req, res) => {
+      const userInfo = req.body;
+      // console.log(userInfo);
+      const filter = { email: userInfo.email };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          email: userInfo.email,
+          name: userInfo.name,
+          photoUrl: userInfo.photoUrl,
+        },
+      };
+      const result = await userCollection.updateOne(filter, updateDoc, options);
+      res.send(result);
+    });
+    //insert user post
+    app.post("/post", async (req, res) => {
+      const post = req.body;
+      const result = await postCollection.insertOne(post);
+      res.send(result);
+    });
 
-      //insert userinfo into the database
-      app.post("/storeUser", async (req, res) => {
-        const userInfo = req.body;
-        // console.log(userInfo);
-        const filter = { email: userInfo.email };
-        const options = { upsert: true };
-        const updateDoc = {
-          $set: {
-            email: userInfo.email,
-            name: userInfo.name,
-            photoUrl: userInfo.photoUrl,
-          },
-        };
-       const result = await userCollection.updateOne(
-         filter,
-         updateDoc,
-         options
-       );
-        res.send(result);
-      });
+    //get all posts
+    app.get("/posts", async (req, res) => {
+      const query = {};
+      const cursor = postCollection.find(query).sort({ milliseconds: -1 });
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
-    }
-
-    catch{
-
-    }
+    //get specific post
+    app.get("/media/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = {_id: ObjectId(id)};
+      const result = await postCollection.findOne(query);
+      res.send(result);
+    });
+  } catch {}
 }
-run().catch(err => console.log(err))
+run().catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
   res.send("api found");
